9cb47f8d3aa6572572b16e5a5107d526dc3c95a2
Sophie <totalsophie@gmail.com>
2023-05-21 21:47:31 +0300

    Add the initial files for the project

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Here with me, teehee
         </p>
         <a
           className="App-link"
